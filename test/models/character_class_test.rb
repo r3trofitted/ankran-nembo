27111355cc186ed8160ba470e3a7db9254ff5818
Ronan Limon Duparcmeur <ronan@2-45.pm>
2017-06-25 09:43:50 +0200

    Basics: character classes

---
+++ test/models/character_class_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+require "dice"
+
+class CharacterClassTest < ActiveSupport::TestCase
+  test "by default, a CharacterClass has a die type of :d8" do
+    assert_equal :d8, CharacterClass.new.hit_die_type
+  end
+  
+  test "when extending a Character, a CharacterClass redefines its `#hit_dice`" do
+    fighter = CharacterClass.new hit_die_type: :d10
+    fafhrd = Character.new level: 3
+    
+    refute_equal Dice("3d10"), fafhrd.hit_dice
+    fafhrd.extend(fighter)
+    assert_equal Dice("3d10"), fafhrd.hit_dice
+  end
+end
