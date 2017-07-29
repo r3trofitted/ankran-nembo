a0d0d4060071bda4b139338c78cea187fad422a5
Ronan Limon Duparcmeur <ronan@2-45.pm>
2017-07-29 10:36:08 +0200

    WIP: Character classes

--- app/models/character_classes/barbarian.rb
+++ app/models/character_classes/barbarian.rb
@@ -3,6 +3,7 @@ module CharacterClasses
     hit_die_type: :d12,
     proficiencies: [:light_armor, :medium_armor, :shields,
                     :simple_weapons, :martial_weapons,
-                    :strength_saving_throw, :constitution_saving_throw]
+                    :strength_saving_throw, :constitution_saving_throw,
+                    Pick.choose(2, from: [:animal_handling, :athletics, :intimidation, :nature, :perception, :survival])]
   )
 end
