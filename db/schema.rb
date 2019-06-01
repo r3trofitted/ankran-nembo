38d1f19859884b9c18da31f5987ac40478ed0131
Ronan Limon Duparcmeur <ronan@2-45.pm>
2019-06-01 18:09:35 +0200

    `CharacterCreation` doesn't need to be an AR model
    For now, at least.

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729172849) do
+ActiveRecord::Schema.define(version: 2017_07_29_172849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "character_creations", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
