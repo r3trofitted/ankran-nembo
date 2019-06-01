6ced60df8d67ded555ecac68332969d2db1ffc88
Ronan Limon Duparcmeur <ronan@2-45.pm>
2019-06-01 18:27:13 +0200

    Updading to Rails 6.0.0.rc1
    Everything went smoothly. All I needed to do was`bundle update`,
    `rails app:update`, and a few changes to `application.rb`.

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,14 @@ Bundler.require(*Rails.groups)
 
 module AnkranNembo
   class Application < Rails::Application
-    config.load_defaults 5.2
-    config.autoload_paths << "#{Rails.root}/lib"
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+    config.autoload_paths << "#{config.root}/lib"
+    config.add_autoload_paths_to_load_path = false
+    
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
