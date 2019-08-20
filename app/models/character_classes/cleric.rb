8b6c42a3d461c74ebba691ca111e31dfc7f761ba
Ronan Limon Duparcmeur <ronan@2-45.pm>
2019-08-20 13:34:55 +0200

    fixup! WIP: Character classes

--- app/models/character_classes/cleric.rb
+++ app/models/character_classes/cleric.rb
@@ -2,7 +2,7 @@ module CharacterClasses
   Cleric = CharacterClass.new(
     hit_die_type: :d8,
     proficiencies: [:light_armor, :medium_armor,
-                    :simple_weapons
+                    :simple_weapons,
                     :wisdom_saving_throw, :charisma_saving_throw,
                     Pick.choose(2, from: [:history, :insight, :medicine, :persuasion, :religion])]
   )
